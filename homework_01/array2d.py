cd1c0852d20940714e77fe44878fb998efa38ee6
Marcin <marcio.moch@gmail.com>
2020-01-12 00:21:31 +0100

    Updated create2DArray util

--- homework_01/array2d.py
+++ homework_01/array2d.py
@@ -2,5 +2,5 @@ from typing import List
 
 Array2d = List[List[int]]
 
-def create_empty_2d_array(row: int, col: int) -> Array2d:
-  return [[0 for x in range(col)] for y in range(row)]
+def create_empty_2d_array(row: int, col: int, val: int = 0) -> Array2d:
+  return [[val for x in range(col)] for y in range(row)]
